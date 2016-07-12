2d6b9a29835f9090500fa69ffdcbf2787f7077f4
prashantmathias <mathiasaudio@gmail.com>
2016-07-12 12:09:12 +0100

    completed step 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,12 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'increase balance by amount added' do
+    amount = 20
+    subject.top_up(amount)
+    expect(subject.balance).to eq amount
+  end
+
+
+
 end
