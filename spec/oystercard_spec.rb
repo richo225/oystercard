94119351c4a20a98d0e134d3b608fbaeccea436f
ogirginc <ogirginc@gmail.com>
2016-07-12 12:41:47 +0100

    Complete Ch4: Initialize #balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  describe '#balance' do
+    it 'should checks that a new card has a balance' do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it 'should have default value of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 
 end
