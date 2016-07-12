0c3fd5fdacbfd7c319f8a1eef8567b0ebe4fba62
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-12 18:15:19 +0100

    Added touch in and touch out with in journey predicate

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,22 +6,36 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it 'increase balance by amount added' do
-    amount = 20
-    subject.top_up(amount)
-    expect(subject.balance).to eq amount
+  context "Modification's to balance" do
+    before do
+      subject.top_up(Oystercard::BALANCE_LIMIT)
+    end
+
+    it 'raise error if balance exceeds max amount' do
+      max_balance = Oystercard::BALANCE_LIMIT
+      expect{ subject.top_up(1) }.to raise_error("Exceeds max allowed amount of #{max_balance}")
+    end
+
+    it 'should deduct amount from balance' do
+      expect{ subject.deduct(20) }.to change{ subject.balance }.by -20
+    end
   end
 
-  it 'raise error if balance exceeds max amount' do
-    max_balance = Oystercard::BALANCE_LIMIT
-    subject.top_up(max_balance)
-    expect{ subject.top_up(1) }.to raise_error("Exceeds max allowed amount of #{max_balance}")
-  end
+  context 'Touch functionality in/out' do
 
-  it 'should deduct amount from balance' do
-    subject.top_up(80)
-    expect{ subject.deduct(20) }.to change{ subject.balance }.by -20
-  end
+    it 'should not be in journey when initiated' do
+      expect(subject).not_to be_in_journey
+    end
 
+    it 'should set card to in journey when touched in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'should set card to NOT be in journey when touched out' do
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 
 end
