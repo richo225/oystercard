66555b94b57da35668459be4a8cf8f84a971846a
prashantmathias <mathiasaudio@gmail.com>
2016-07-15 12:35:56 +0100

    partially completed ch15 extracting Journey log

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,7 +1,17 @@
-
+require_relative "journey"
 
 class JourneyLog
 
+  attr_reader :journey_class, :new_journey
+
+  def initialize(journey_class: Journey.new)
+    @journey_class = journey_class
+  end
+
+  def start(station)
+
+    @new_journey = Journey.new(entry_station: station)
+  end
 
 
 
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -3,10 +3,21 @@ require "journey_log"
 describe JourneyLog do
 
 
+  #let(:station){ double(:station) }
+  journey = Journey.new(entry_station: "station")
+  subject {described_class.new(journey_class: journey)}
+
+  # it "initializes journey_log with a journey class" do
+  #   expect(subject.journey_class).to eq journey
+  # end
+
+
+  #journey2 = Jorney.new(entry_station: station)
+
   it "starts a journey with an entry station" do
-
+  subject.start("station")
+    expect(subject.new_journey).to eq journey
   end
-
 
 
 
