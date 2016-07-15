d7207b445bf3ec431b01fddc41082290b2477959
prashantmathias <mathiasaudio@gmail.com>
2016-07-15 10:42:04 +0100

    modified journey_log

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -2,20 +2,7 @@
 
 class JourneyLog
 
-  def initialize(journey_class: nil)
-    @journey_class = journey_class
 
-  end
-
-  def start(entry_station)
-    @entry_station = entry_station
-  end
-
-private
-
-  def current_journey
-    @current_journey
-  end
 
 
 end
